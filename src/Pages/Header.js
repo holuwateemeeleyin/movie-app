5044c3e5792cae6afe1fd04bb0e46c1505841746
unknown <abegundeosamuel@gmail.com>
2022-07-25 19:33:48 +0100

    Header

---
+++ src/Pages/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className='header-container'>
+        <div className='header'>
+            <div className='header-box'>
+                <div className='header-text'>
+                    MyTestApp
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
