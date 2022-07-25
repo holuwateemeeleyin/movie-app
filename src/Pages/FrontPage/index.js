e1e60c0ccc29ac8e65e2cfbedd14216dadc23e9a
unknown <abegundeosamuel@gmail.com>
2022-07-25 23:37:46 +0100

    Added Image for mobile view

--- src/Pages/FrontPage/index.js
+++ src/Pages/FrontPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './FrontPage.css'
 import FrontImage from '../../Assets/frontImage.png'
+import MobileImage from '../../Assets/ipadImage.png'
 export default function FrontPage() {
   return (
     <div className='frontview-container'>
-        <img src={FrontImage} alt={FrontImage} width='100%'/>
+        <img src={FrontImage} alt={FrontImage} width='100%' className='laptop-image'/>
+        <img src={MobileImage} alt={MobileImage} width='100%' className='mobile'/>
         <div className='frontview-text'>
             Watch something incredible.
         </div>
