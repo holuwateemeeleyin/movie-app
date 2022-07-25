ef5cd9ac5d56c7fbbc382173090e0726f8189086
unknown <abegundeosamuel@gmail.com>
2022-07-25 20:50:39 +0100

    Passed carts as props

--- src/Pages/Movie/index.js
+++ src/Pages/Movie/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import MovieLists from './MovieLists'
 import './Movies.css'
-export default function Movie({ movies }) {
+export default function Movie({ movies, carts, cartHandler }) {
+    console.log(carts);
     return (
         <div className='movie-container'>
             <div className='movie-header'>
@@ -9,11 +10,18 @@ export default function Movie({ movies }) {
             </div>
             <div className='movie-list'>
                     {movies ? movies.map(movie => (
-                        <MovieLists key={movie.imdbID} movie={movie}/>
+                        <MovieLists key={movie.imdbID} movie={movie} cartHandler={cartHandler}/>
                     )): 'Use the SearchBox to view Movies'}
             </div>
             <div className='movie-header'>
-                Cart
+                Carts
+            </div>
+            <div className='movie-list'>
+            {
+                    carts ? carts.map(movie =>(
+                        <MovieLists key={movie.imdbID} movie={movie}/>
+                    )) :'No Movie Added to Carts yet'
+                }
             </div>
         </div>
     )
