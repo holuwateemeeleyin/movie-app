0d4c4e9cd7d1f8bbe3b02c65deba254de6d9d1da
unknown <abegundeosamuel@gmail.com>
2022-07-25 20:51:21 +0100

    Cards text

--- src/Pages/Movie/MovieLists.js
+++ src/Pages/Movie/MovieLists.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Movies.css'
-export default function MovieLists({movie}) {
+export default function MovieLists({movie, cartHandler}) {
     return (
         <div className='movie-card'>
             <img src={movie.Poster} alt={movie.title} width='100%'/>
+            <div className='movie-title'>{movie.Title}</div>
+            <div className='cart' onClick={()=>{cartHandler(movie)}}>Add To Cart </div>
         </div>
     )
 }
