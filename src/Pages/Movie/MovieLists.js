1228e40e622706e91c368a3c8a2c5e8a2eab44a0
unknown <abegundeosamuel@gmail.com>
2022-07-26 19:36:37 +0100

    remove cart function

--- src/Pages/Movie/MovieLists.js
+++ src/Pages/Movie/MovieLists.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Movies.css'
-export default function MovieLists({movie, cartHandler}) {
+export default function MovieLists({movie, cartHandler, removeCart}) {
     return (
         <div className='movie-card'>
             <img src={movie.Poster} alt={movie.title} width='100%'/>
             <div className='movie-title'>{movie.Title}</div>
             <div className='cart' onClick={()=>{cartHandler(movie)}}>Add To Cart </div>
+            <div className='remove-cart' onClick={()=>{removeCart(movie)}}>Remove Cart</div>
         </div>
     )
 }
